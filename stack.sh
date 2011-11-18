9f33c13e6d8b1c499b3587635c67e060176f3f7a
Don Dugger <n0ano@astarte.(none)>
2011-11-18 10:23:52 -0700

    Enhanced identification of HOST_IP and eth device - V2
    Update of the prior commit to simplify the identification (only run
    `ifconfig' once and save the results in a shell variable for easy
    access).
    
    Also, add the ability to search the `nova.conf' file for the host IP
    address and interface name if this is the second time the script has
    been run (nova networking removes the IP address from the public
    interface, leaving the `nova.conf' file as the only place to find it
    again).

--- stack.sh
+++ stack.sh
@@ -176,19 +176,23 @@ SCHEDULER=${SCHEDULER:-nova.scheduler.simple.SimpleScheduler}
 
 # Use the eth0 IP unless an explicit is set by ``HOST_IP`` environment variable
 # Note similar code for finding the ``PUBLIC_INTERFACE`` and ``FLAT_INTERFACE``
-#   variables.  All of these 3 variables will be set to the first NIC that has an
-#   IP address.
+#   variables.  All of these 3 variables will be set to the first NIC that has
+#   an IP address.
+NIC=`LC_ALL=C ifconfig | awk '/^eth/{ NAME = $1; getline ; if ($1 == "inet") { sub("addr:", "", $2); print NAME, $2; exit}}'`
 if [ ! -n "$HOST_IP" ]; then
-    HOST_IP=`LC_ALL=C ifconfig | awk '/^eth/{ getline ; if ($1 == "inet") { sub("addr:", "", $2); print $2; exit}}'`
+    HOST_IP=`expr "$NIC" : '.* \(.*\)'`
     if [ "$HOST_IP" = "" ]; then
-        echo "Could not determine host ip address."
-        echo "If this is not your first run of stack.sh, it is "
-        echo "possible that nova moved your eth ip address to the FLAT_NETWORK_BRIDGE."
-        echo "Please specify your HOST_IP, PUBLIC_INTERFACE and FLAT_INTERFACE in your localrc."
-	echo "(Correct values should be in $NOVA_DIR/bin/nova.conf)"
-        exit 1
+	HOST_IP=`sed -n -e '/^--my_ip/s/--my_ip=//p' $NOVA_DIR/bin/nova.conf`
+	if [ "$HOST_IP" = "" ]; then
+	    echo "Weird network setup, could not determine host ip address."
+	    echo "Need to manually specify HOST_IP, PUBLIC_INTERFACE and FLAT_INTERFACE"
+	    exit 1
+	fi 
+	NIC=`sed -n -e '/^--public_interface/s/--public_interface=//p' $NOVA_DIR/bin/nova.conf`
+	NIC="$NIC "
     fi
 fi
+NIC=`expr "$NIC" : '\(.*\) '`
 
 # Service startup timeout
 SERVICE_TIMEOUT=${SERVICE_TIMEOUT:-60}
@@ -236,9 +240,7 @@ function read_password {
 # FIXME: more documentation about why these are important flags.  Also
 # we should make sure we use the same variable names as the flag names.
 
-if [ ! -n "$PUBLIC_INTERFACE" ]; then
-    PUBLIC_INTERFACE=`LC_ALL=C ifconfig | awk '/^eth/{ NIC = $1; getline ; if ($1 == "inet") { print NIC; exit}}'`
-fi
+PUBLIC_INTERFACE=${PUBLIC_INTERFACE:-$NIC}
 FIXED_RANGE=${FIXED_RANGE:-10.0.0.0/24}
 FIXED_NETWORK_SIZE=${FIXED_NETWORK_SIZE:-256}
 FLOATING_RANGE=${FLOATING_RANGE:-172.24.4.224/28}
@@ -266,9 +268,7 @@ MULTI_HOST=${MULTI_HOST:-False}
 # devices other than that node, you can set the flat interface to the same
 # value as ``FLAT_NETWORK_BRIDGE``.  This will stop the network hiccup from
 # occurring.
-if [ ! -n "$FLAT_INTERFACE" ]; then
-    FLAT_INTERFACE=`LC_ALL=C ifconfig | awk '/^eth/{ NIC = $1; getline ; if ($1 == "inet") { print NIC; exit}}'`
-fi
+FLAT_INTERFACE=${FLAT_INTERFACE:-$NIC}
 
 ## FIXME(ja): should/can we check that FLAT_INTERFACE is sane?
 
