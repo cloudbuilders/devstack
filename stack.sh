adba6bcf232c3a936bf61595cacdc6d6dd980d58
Ghe Rivero <ghe@debian.org>
2011-12-22 09:17:17 +0100

    Update stack.sh

--- stack.sh
+++ stack.sh
@@ -376,7 +376,7 @@ read_password SERVICE_TOKEN "ENTER A SERVICE_TOKEN TO USE FOR THE SERVICE ADMIN 
 read_password ADMIN_PASSWORD "ENTER A PASSWORD TO USE FOR HORIZON AND KEYSTONE (20 CHARS OR LESS)."
 
 LOGFILE=${LOGFILE:-"$PWD/stack.sh.$$.log"}
-(
+
 # So that errors don't compound we exit on any errors so you see only the
 # first error that occurred.
 trap failed ERR
