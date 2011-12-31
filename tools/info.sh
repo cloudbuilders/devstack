6143324eea7b6aa8edf35eeaa6f1735c5e1ccef6
Dean Troyer <dtroyer@gmail.com>
2011-12-30 23:56:06 -0600

    More cleanupification

--- tools/info.sh
+++ tools/info.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-#
 # info.sh - Produce a report on the state of devstack installs
 #
+# Output fields are separated with '|' chars
+# Output types are git,localrc,os,pip,pkg:
+#   git|<project>|<branch>[<shaq>]
+#   localtc|<var>=<value>
+#   os|<var>=<value>
+#   pip|<package>|<version>
+#   pkg|<package>|<version>
 
 function usage {
     echo "$0 - Report on the devstack configuration"
@@ -16,7 +22,7 @@ fi
 
 # Keep track of the current directory
 TOOLS_DIR=$(cd $(dirname "$0") && pwd)
-TOP_DIR=`cd $TOOLS_DIR/..; pwd`
+TOP_DIR=$(cd $TOOLS_DIR/..; pwd)
 cd $TOP_DIR
 
 # Source params
@@ -29,24 +35,23 @@ if [[ ! -d $FILES ]]; then
     exit 1
 fi
 
+# Repos
+# -----
+
 # git_report <dir>
 function git_report() {
     local dir=$1
-    local ref=""
-    local head=""
+    local proj ref branch head
     if [[ -d $dir/.git ]]; then
         pushd $dir >/dev/null
-        ref=`git symbolic-ref HEAD`
+        proj=$(basename $dir)
+        ref=$(git symbolic-ref HEAD)
         branch=${ref##refs/heads/}
-        head=`git show-branch --sha1-name $branch | cut -d' ' -f1`
-        proj=`basename $dir`
+        head=$(git show-branch --sha1-name $branch | cut -d' ' -f1)
         echo "git|${proj}|${branch}${head}"
         popd >/dev/null
     fi
 }
-
-# Repos
-# -----
 
 for i in $DEST/*; do
     if [[ -d $i ]]; then
@@ -90,10 +95,13 @@ GetOSInfo() {
     fi
 }
 
-# - We are going to install packages only for the services needed.
+GetOSInfo
+
+# Packages
+# --------
+
+# - We are going to check packages only for the services needed.
 # - We are parsing the packages files and detecting metadatas.
-#  - If there is a NOPRIME as comment mean we are not doing the install
-#    just yet.
 #  - If we have the meta-keyword dist:DISTRO or
 #    dist:DISTRO1,DISTRO2 it will be installed only for those
 #    distros (case insensitive).
@@ -124,7 +132,7 @@ function get_packages() {
     for file in ${file_to_parse}; do
         local fname=${FILES}/apts/${file}
         local OIFS line package distros distro
-        [[ -e $fname ]] || { echo "missing: $fname"; exit 1 ;}
+        [[ -e $fname ]] || { echo "missing: $fname"; exit 1; }
 
         OIFS=$IFS
         IFS=$'\n'
@@ -144,37 +152,49 @@ function get_packages() {
     done
 }
 
-GetOSInfo
-
 for p in $(get_packages); do
-    ver=`dpkg -s $p 2>/dev/null | grep '^Version: ' | cut -d' ' -f2`
+    ver=$(dpkg -s $p 2>/dev/null | grep '^Version: ' | cut -d' ' -f2)
     echo "pkg|${p}|${ver}"
 done
 
+# Pips
+# ----
+
+function get_pips() {
+    cat $FILES/pips/* | uniq
+}
+
+# Pip tells us what is currently installed
 FREEZE_FILE=$(mktemp --tmpdir freeze.XXXXXX)
 pip freeze >$FREEZE_FILE 2>/dev/null
-for p in $(cat $FILES/pips/* | uniq ); do
+
+# Loop through our requirements and look for matches
+for p in $(get_pips); do
     [[ "$p" = "-e" ]] && continue
-    if $(echo $p | grep -q +http); then
-        p=$(echo $p | cut -d'+' -f2 | cut -d'#' -f1)
+    if [[ "$p" =~ \+?([^#]*)#? ]]; then
+         # Get the URL from a remote reference
+         p=${BASH_REMATCH[1]}
     fi
     line="`grep -i $p $FREEZE_FILE`"
     if [[ -n "$line" ]]; then
         if [[ "$line" =~ \+(.*)@(.*)#egg=(.*) ]]; then
+            # Handle URLs
+            p=${BASH_REMATCH[1]}
+            ver=${BASH_REMATCH[2]}
+        elif [[ "$line" =~ (.*)[=\<\>]=(.*) ]]; then
+            # Normal pip packages
             p=${BASH_REMATCH[1]}
             ver=${BASH_REMATCH[2]}
         else
-            if [[ "$line" =~ (.*)[=\<\>]=(.*) ]]; then
-                p=${BASH_REMATCH[1]}
-                ver=${BASH_REMATCH[2]}
-            else
-                #echo "unknown: $p"
-                continue
-            fi
+            # Unhandled format in freeze file
+            #echo "unknown: $p"
+            continue
         fi
         echo "pip|${p}|${ver}"
-    #else
+    else
+        # No match in freeze file
         #echo "unknown: $p"
+        continue
     fi
 done
 
@@ -183,11 +203,11 @@ rm $FREEZE_FILE
 # localrc
 # -------
 
+# Dump localrc with 'localrc|' prepended and comments and passwords left out
 if [[ -r $TOP_DIR/localrc ]]; then
-    echo ""
-    echo "localrc:"
     sed -e '
         /PASSWORD/d;
         /^#/d;
+        s/^/localrc\|/;
     ' $TOP_DIR/localrc | sort
 fi
