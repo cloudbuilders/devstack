16a8563e37eca7da51b67b541c4da2e5ed9682c1
jakedahn <jake@ansolabs.com>
2012-01-23 23:50:39 -0800

    converting to nova
    Change-Id: Id915e5642725888b6df50729abac2fd7671399ef

--- tools/get_certs.sh
+++ tools/get_certs.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-# This can be switched to use the NOVA client once python-novaclient is updated
-
 pushd $(cd $(dirname "$0")/.. && pwd)
 source ./openrc
 
-RESULT=`curl -s -d  "{\"auth\":{\"passwordCredentials\": {\"username\": \"$NOVA_USERNAME\", \"password\": \"$NOVA_PASSWORD\"}, \"tenantName\": \"$NOVA_PROJECT_ID\"}}" -H "Content-type: application/json" http://$HOST_IP:5000/v2.0/tokens`
-TOKEN=`echo $RESULT | python -c "import sys; import json; res = json.loads(sys.stdin.read()); print res['access']['token']['id'];"`
-ENDPOINT=`echo $RESULT | python -c "import sys; import json; res = json.loads(sys.stdin.read()); print [x for x in res['access']['serviceCatalog'] if x['type'] == 'compute'][0]['endpoints'][0]['publicURL'];"`
+nova x509-create-cert
+nova x509-get-root-cert
 
-RESULT=`curl -s -X POST -H "x-auth-token: $TOKEN" $ENDPOINT/os-certificates`
-
-echo $RESULT | python -c "import sys; import json; res = json.loads(sys.stdin.read()); print res['certificate']['data'];" > cert.pem
-echo $RESULT | python -c "import sys; import json; res = json.loads(sys.stdin.read()); print res['certificate']['private_key'];" > pk.pem
-
-curl -s -H "x-auth-token: $TOKEN" $ENDPOINT/os-certificates/root | python -c "import sys; import json; res = json.loads(sys.stdin.read()); print res['certificate']['data'];" > cacert.pem
 popd
