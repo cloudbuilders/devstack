59b3f402cd9a997836b03560c7d31cf96304ca1e
Ronald Bradford <ronald.bradford@gmail.com>
2012-01-26 15:25:11 -0500

    Added check for watch installed on system

--- stack.sh
+++ stack.sh
@@ -97,7 +97,7 @@ function apt_get() {
 }
 
 # Check to see if we are already running a stack.sh
-if screen -ls | egrep -q "[0-9].stack"; then
+if [ ! -z `which screen` ] && screen -ls | egrep -q "[0-9].stack"; then
     echo "You are already running a stack.sh session."
     echo "To rejoin this session type 'screen -x stack'."
     echo "To destroy this session, kill the running screen."
